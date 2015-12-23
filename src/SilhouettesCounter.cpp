872c93f8c5a9674616d4cb7e62facb187db60d09
iardelyan <ardelyan.i@gmail.com>
2015-12-23 23:41:20 +0200

    Fix some code bugs, formatting and function names

--- src/SilhouettesCounter.cpp
+++ src/SilhouettesCounter.cpp
@@ -5,96 +5,112 @@
 
 using namespace std;
 
-//function prototypes
+const int MIN_AREA_OF_SILHOUETTE = 100;// the minimum number of pixels required to determine the silhouette
+const int BACKGROUND_COLOR = 13750737; // color for background (HEX #d1d1d1)
 
-int pixCounter = 100;// the minimum number of pixels required to determine the silhouette
+//function prototypes
 
 //@param imgName - name of the input image
 int countSilhouettes(string imgName);
 
-//@param bitMap - a bitmap of image
-//@param imgBuffer - matrix of colored pixels
-//@param imgWidth - width of the image
-//@param imgHeight - height of the image
-void binarization(int **bitMap, GBufferedImage* imgBuffer, int imgWidth, int imgHeight);
+/*
+*@param bitMap - a bitmap of image
+*@param imgBuffer - matrix of colored pixels
+*@param imgWidth - width of the image
+*@param imgHeight - height of the image
+*/
+void imageBinarization(int **bitMap, GBufferedImage* imgBuffer, int imgWidth, int imgHeight);
 
-//@param x, y - coordinates of the bitmap
-//@param mark - label used for marking pixels of the silhouettes
-//@param newMark - label used for separation pixels of the silhouettes
-void marking(int x, int y, int mark, int newMark, int **bitMap, int imgWidth, int imgHeight);
+/*
+*@param x, y - coordinates of the bitmap
+*@param pixelValue - label used for marking pixels of the silhouettes
+*@param newPixelValue - label used for separation pixels of the silhouettes
+*@param silhouetteSize - the minimum size of the silhouette to be taken into account
+*/
+void connectedRegionsSearch(int x, int y, int pixelValue, int newPixelValue, int **bitMap, int imgWidth, int imgHeight, int &silhouetteSize);
 
 int main() {
     string imgName;
     cout << "Please, enter name of the image: ";
     cin >> imgName;
-    cout << "Result: There is(are) " << countSilhouettes(imgName) << " silhouette(s) on the image." << endl;
+    cout << "silhouette: There is/are " << countSilhouettes(imgName) << " silhouette/s on the image." << endl;
     return 0;
 }
 
-int countSilhouettes(string imgName){
-    GImage* image;
+int countSilhouettes(string imgName) {
+    GImage *image;
     image = new GImage(imgName);
-    GWindow gw(image->getWidth(),image->getHeight());// window size is the size of the image
+    //object window
+    GWindow gw(image->getWidth(), image->getHeight());// window size is the size of the image
     gw.add(image);
-    GBufferedImage* imgBuffer = new GBufferedImage(0, 0, image->getWidth(), image->getHeight());//matrix size is the size of the image
+    //buffer for image pixel reading
+    GBufferedImage *imgBuffer = new GBufferedImage(0, 0, image->getWidth(), image->getHeight()); //matrix size is the size of the image
     imgBuffer->load(imgName);
-    //image width and height.
+    //image width and height
     int imgHeight = (int)imgBuffer->getHeight();
     int imgWidth = (int)imgBuffer->getWidth();
     //create a bitmap array
     int **bitMap = new int *[imgWidth];
-    for(int x = 0; x < imgWidth; x++){
+    
+    for (int x = 0; x < imgWidth; x++) {
         bitMap[x] = new int[imgHeight];
     }
-    // binarization of the image
-    binarization(bitMap, imgBuffer, imgWidth, imgHeight);
-    int mark = 1;
-    int newMark = 1;
-    int result = 0;// silhouettes counter
-    for(int x = 1; x < imgWidth; x++){
-        for(int y = 1; y < imgHeight; y++){
-            if(bitMap[x][y] == 1){
-                if (pixCounter >=100){ //if the object of more than 100 pixels, we assume that this is silhouette
-                newMark++;// when out of recurcive marking cycle - new mark
-                result++;// and result will be increased by one
+    
+    imageBinarization(bitMap, imgBuffer, imgWidth, imgHeight);// binarization of the image
+    int pixelValue = 1; // new value of the pixel
+    int newPixelValue = 1;// old value of the pixel
+    int silhouettesOnImg = 0;// silhouettes counter
+    
+    for (int x = 1; x < imgWidth; x++) {
+        for (int y = 1; y < imgHeight; y++) {
+            if (bitMap[x][y] == 1) {
+                newPixelValue++;
+                int silhouetteSize = 0;
+                connectedRegionsSearch(x, y, pixelValue, newPixelValue, bitMap, imgWidth, imgHeight, silhouetteSize);
+                
+                // if the size of the selected area is less than MIN_AREA_OF_SILHOUETTE,
+                // we do not consider this area a separate silhouette
+                if (silhouetteSize >= MIN_AREA_OF_SILHOUETTE) {
+                    silhouettesOnImg++;
                 }
-                pixCounter = 0;//zero out after finding each silhouette
-                marking(x, y, mark, newMark, bitMap, imgWidth, imgHeight);
             }
         }
     }
+    
     // clean memory
-    for(int x = 0; x < imgWidth; x++){
+    for (int x = 0; x < imgWidth; x++) {
         delete[]bitMap[x];
     }
+    
     delete []bitMap;
     delete image;
     delete imgBuffer;
-    return result;
+    return silhouettesOnImg;
 }
-// binarization of image
-void binarization(int **bitMap, GBufferedImage* imgBuffer, int imgWidth, int imgHeight){
-    int backColor = 13750737; // color for background (HEX #d1d1d1)
-    int pixRGB = 0;
-    for(int y = 0; y < imgHeight; y++){
-        for(int x = 0; x < imgWidth; x++){
-            pixRGB = imgBuffer->getRGB(x, y);
-            if(pixRGB > backColor){
+
+void imageBinarization(int **bitMap, GBufferedImage* imgBuffer, int imgWidth, int imgHeight){
+    int pixelColor = 0;
+    for (int y = 1; y < imgHeight; y++) {
+        for (int x = 1; x < imgWidth; x++) {
+            pixelColor = imgBuffer->getRGB(x, y);
+            if (pixelColor > BACKGROUND_COLOR) {
                 bitMap[x][y] = 0;
-            }else{
+            } else {
                 bitMap[x][y] = 1;
             }
         }
     }
 }
+
 //recursive function put unique numeric layer on each found separated object on image.
-void marking(int x, int y, int mark, int newMark, int **bitMap, int imgWidth, int imgHeight){
-    if(x >= 0 && x < imgWidth && y >= 0 && y < imgHeight && bitMap[x][y] == mark && bitMap[x][y] != newMark){
-        pixCounter++;
-        bitMap[x][y] = newMark;
-        marking(x, y+1, mark, newMark, bitMap, imgWidth, imgHeight);
-        marking(x, y-1, mark, newMark, bitMap, imgWidth, imgHeight);
-        marking(x-1, y, mark, newMark, bitMap, imgWidth, imgHeight);
-        marking(x+1, y, mark, newMark, bitMap, imgWidth, imgHeight);
+void connectedRegionsSearch(int x, int y, int pixelValue, int newPixelValue, int **bitMap, int imgWidth, int imgHeight, int & silhouetteSize) {
+    
+    if (x >= 0 && x < imgWidth && y >= 0 && y < imgHeight && bitMap[x][y] == pixelValue && bitMap[x][y] != newPixelValue) {
+        silhouetteSize++;
+        bitMap[x][y] = newPixelValue;
+        connectedRegionsSearch(x, y+1, pixelValue, newPixelValue, bitMap, imgWidth, imgHeight, silhouetteSize);
+        connectedRegionsSearch(x, y-1, pixelValue, newPixelValue, bitMap, imgWidth, imgHeight, silhouetteSize);
+        connectedRegionsSearch(x-1, y, pixelValue, newPixelValue, bitMap, imgWidth, imgHeight, silhouetteSize);
+        connectedRegionsSearch(x+1, y, pixelValue, newPixelValue, bitMap, imgWidth, imgHeight, silhouetteSize);
     }
 }
