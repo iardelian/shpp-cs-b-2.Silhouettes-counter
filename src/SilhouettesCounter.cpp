a27e3f076201051fb05d115259c6f86d4b2709b9
iardelyan <ardelyan.i@gmail.com>
2016-01-22 14:17:38 +0200

    Final

--- src/SilhouettesCounter.cpp
+++ src/SilhouettesCounter.cpp
@@ -5,7 +5,7 @@
 
 using namespace std;
 
-const int MIN_AREA_OF_SILHOUETTE = 100;// the minimum number of pixels required to determine the silhouette
+const int MIN_SILHOUETTE_SIZE = 100;// the minimum number of pixels required to determine the silhouette
 const int BACKGROUND_COLOR = 13750737; // color for background (HEX #d1d1d1)
 
 //function prototypes
@@ -19,23 +19,15 @@ int countSilhouettes(string imgName);
 *@param imgWidth - width of the image
 *@param imgHeight - height of the image
 */
-void imageBinarization(int **bitMap, GBufferedImage* imgBuffer, int imgWidth, int imgHeight);
+void binarizationImage(int **bitMap, GBufferedImage* imgBuffer, int imgWidth, int imgHeight);
 
 /*
 *@param x, y - coordinates of the bitmap
-*@param pixelValue - label used for marking pixels of the silhouettes
-*@param newPixelValue - label used for separation pixels of the silhouettes
+*@param oldValue - label used for marking pixels of the silhouettes
+*@param newValue - label used for separation pixels of the silhouettes
 *@param silhouetteSize - the minimum size of the silhouette to be taken into account
 */
-void connectedRegionsSearch(int x, int y, int pixelValue, int newPixelValue, int **bitMap, int imgWidth, int imgHeight, int &silhouetteSize);
-
-int main() {
-    string imgName;
-    cout << "Please, enter name of the image: ";
-    cin >> imgName;
-    cout << "silhouette: There is/are " << countSilhouettes(imgName) << " silhouette/s on the image." << endl;
-    return 0;
-}
+void fillRegion(int x, int y, int oldValue, int newValue, int **bitMap, int imgWidth, int imgHeight, int &silhouetteSize);
 
 int countSilhouettes(string imgName) {
     GImage *image;
@@ -51,44 +43,44 @@ int countSilhouettes(string imgName) {
     int imgWidth = (int)imgBuffer->getWidth();
     //create a bitmap array
     int **bitMap = new int *[imgWidth];
-    
+
     for (int x = 0; x < imgWidth; x++) {
         bitMap[x] = new int[imgHeight];
     }
-    
-    imageBinarization(bitMap, imgBuffer, imgWidth, imgHeight);// binarization of the image
-    int pixelValue = 1; // new value of the pixel
-    int newPixelValue = 1;// old value of the pixel
+
+    binarizationImage(bitMap, imgBuffer, imgWidth, imgHeight);// binarization of the image
+    int oldValue = 1; // new value of the pixel
+    int newValue = 1;// old value of the pixel
     int silhouettesOnImg = 0;// silhouettes counter
-    
+
     for (int x = 1; x < imgWidth; x++) {
         for (int y = 1; y < imgHeight; y++) {
             if (bitMap[x][y] == 1) {
-                newPixelValue++;
+                newValue++;
                 int silhouetteSize = 0;
-                connectedRegionsSearch(x, y, pixelValue, newPixelValue, bitMap, imgWidth, imgHeight, silhouetteSize);
-                
-                // if the size of the selected area is less than MIN_AREA_OF_SILHOUETTE,
+                fillRegion(x, y, oldValue, newValue, bitMap, imgWidth, imgHeight, silhouetteSize);
+
+                // if the size of the selected area is less than MIN_SILHOUETTE_SIZE,
                 // we do not consider this area a separate silhouette
-                if (silhouetteSize >= MIN_AREA_OF_SILHOUETTE) {
+                if (silhouetteSize >= MIN_SILHOUETTE_SIZE) {
                     silhouettesOnImg++;
                 }
             }
         }
     }
-    
+
     // clean memory
     for (int x = 0; x < imgWidth; x++) {
         delete[]bitMap[x];
     }
-    
+
     delete []bitMap;
     delete image;
     delete imgBuffer;
     return silhouettesOnImg;
 }
 
-void imageBinarization(int **bitMap, GBufferedImage* imgBuffer, int imgWidth, int imgHeight){
+void binarizationImage(int **bitMap, GBufferedImage* imgBuffer, int imgWidth, int imgHeight){
     int pixelColor = 0;
     for (int y = 1; y < imgHeight; y++) {
         for (int x = 1; x < imgWidth; x++) {
@@ -102,15 +94,23 @@ void imageBinarization(int **bitMap, GBufferedImage* imgBuffer, int imgWidth, in
     }
 }
 
-//recursive function put unique numeric layer on each found separated object on image.
-void connectedRegionsSearch(int x, int y, int pixelValue, int newPixelValue, int **bitMap, int imgWidth, int imgHeight, int & silhouetteSize) {
-    
-    if (x >= 0 && x < imgWidth && y >= 0 && y < imgHeight && bitMap[x][y] == pixelValue && bitMap[x][y] != newPixelValue) {
+//recursive function that found filling set in the binary plane
+void fillRegion(int x, int y, int oldValue, int newValue, int **bitMap, int imgWidth, int imgHeight, int & silhouetteSize) {
+
+    if (x >= 0 && x < imgWidth && y >= 0 && y < imgHeight && bitMap[x][y] == oldValue && bitMap[x][y] != newValue) {
         silhouetteSize++;
-        bitMap[x][y] = newPixelValue;
-        connectedRegionsSearch(x, y+1, pixelValue, newPixelValue, bitMap, imgWidth, imgHeight, silhouetteSize);
-        connectedRegionsSearch(x, y-1, pixelValue, newPixelValue, bitMap, imgWidth, imgHeight, silhouetteSize);
-        connectedRegionsSearch(x-1, y, pixelValue, newPixelValue, bitMap, imgWidth, imgHeight, silhouetteSize);
-        connectedRegionsSearch(x+1, y, pixelValue, newPixelValue, bitMap, imgWidth, imgHeight, silhouetteSize);
+        bitMap[x][y] = newValue;
+        fillRegion(x, y+1, oldValue, newValue, bitMap, imgWidth, imgHeight, silhouetteSize);
+        fillRegion(x, y-1, oldValue, newValue, bitMap, imgWidth, imgHeight, silhouetteSize);
+        fillRegion(x-1, y, oldValue, newValue, bitMap, imgWidth, imgHeight, silhouetteSize);
+        fillRegion(x+1, y, oldValue, newValue, bitMap, imgWidth, imgHeight, silhouetteSize);
     }
+}
+
+int main() {
+    string imgName;
+    cout << "Please, enter name of the image: ";
+    cin >> imgName;
+    cout << "silhouette: There is/are " << countSilhouettes(imgName) << " silhouette/s on the image." << endl;
+    return 0;
 }
